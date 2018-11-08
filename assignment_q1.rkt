b14ecb944efcdc80d55628584591c2be71b4b2a9
MisterMonocle <T.SIEBERT1@nuigalway.ie>
2018-11-08 20:01:31 +0000

    Add files via upload

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,6 @@
 #lang racket
+(cons 1 2)
+(cons '1(cons '2(cons '3'())))
+(cons (cons "This is a string" 2 )(cons '1(cons '2(cons '3'()))))
+(list (list "This is a string" (list 2 ) (list 1 2 3)))
+(append '("This is a string") '(2) '(1 2 3))
--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -12,6 +12,56 @@
 ;You may delete these comments!
 
 (provide ins_beg)
+(provide ins_end)
+(provide count_top_level)
+(provide count_instances)
+(provide count_instances_tr)
+(provide reduce)
+(provide count_instances_deep)
 
 (define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+  (cons el lst))
+
+(define (ins_end el lst)
+  (cons lst el))
+
+(define (count_top_level lst)
+  (cond ((null? lst) 0)
+        ((not (pair? lst))1)
+        (else (+ (count_top_level (car lst))
+                 (count_top_level (cdr lst))))))
+              
+(define (count_instances lst el)
+      (cond ((null? lst) 0)
+          ((= (car lst) el) (+ 1 (count_instances (cdr lst) el)))
+          (else (count_instances (cdr lst) el))))
+
+(define (count_instances_tr lst el)
+      (cond ((null? lst) 0)
+          ((= (car lst) el) (+ 1 (count_instances (cdr lst) el)))
+          (else (count_instances (cdr lst) el))))
+
+(define (rev_l a b)
+	(cond ( (null? b) a) 
+	( else (rev_l (append (list (car b)) a) (cdr b)))))
+
+(define (reduce pair-fn atom-fn el)
+  (if (pair? el)
+      (pair-fn (reduce pair-fn atom-fn (car el))
+               (reduce pair-fn atom-fn (cdr el)))
+      (atom-fn el)))
+
+(define (count_instances_deep el)
+  (reduce +
+            (lambda (atom) 
+              (if (not (null? atom))
+                  1
+                  0))
+            el))
+
+(ins_beg 'a'(b c d))
+(ins_end '(a b)'(b c d))
+(count_top_level '(1 2 3 (4 5) 6))
+(count_instances '(1 2 3 2 1 2 3) 2)
+
+(count_instances_deep '(1 (1 1) 1))
